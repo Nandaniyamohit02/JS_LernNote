592512b5c07652fda9627232faa9942b40d79fbf
Nandaniyamohit02 <142100951+Nandaniyamohit02@users.noreply.github.com>
2023-08-24 14:46:29 +0000

    info add

---
+++ Basic_Note/Page_01.js
@@ -0,0 +1,37 @@
+console.log("hii there");
+
+const emailId = "admin@gmail.com";
+// const not change data
+
+let Name = "mohit"
+// most use
+
+var nickName = "mohan"
+// don't use  scope problem
+
+console.log(emailId);
+
+console.table([emailId,Name,nickName])
+
+
+// data type             type of
+// String                
+// Number                
+// null                  object
+// Symbol                
+// Object                
+// Boolean               
+// BigInt 
+// undefined             undefined
+
+console.log(typeof(null));
+
+// datatype conversation
+
+// 33  convert in number  output = 33
+// 3a  convert in number  output = NaN
+// true  convert in number  output = 1
+// 12  convert in string  output = 12
+// 0  convert in bool  output = false
+// ""  convert in string  output = false
+
