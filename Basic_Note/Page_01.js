20ff789af5803056f0eaad1beba5cadf6aa9ffe0
Nandaniyamohit02 <142100951+Nandaniyamohit02@users.noreply.github.com>
2023-08-25 08:12:09 +0000

    change data

--- Basic_Note/Page_01.js
+++ Basic_Note/Page_01.js
@@ -1,4 +1,4 @@
-console.log("hii there");
+// console.log("hii there");
 
 const emailId = "admin@gmail.com";
 // const not change data
@@ -9,22 +9,25 @@ let Name = "mohit"
 var nickName = "mohan"
 // don't use  scope problem
 
-console.log(emailId);
+// console.log(emailId);
 
-console.table([emailId,Name,nickName])
+// console.table([emailId,Name,nickName])
 
 
 // data type             type of
-// String                
-// Number                
+// String                string
+// Number                number
 // null                  object
-// Symbol                
-// Object                
-// Boolean               
-// BigInt 
+// Boolean               bool
+// BigInt                bigint
 // undefined             undefined
+// Symbol                symbol
+// Object                object
+// function              function object
+// array                 object
 
-console.log(typeof(null));
+
+// console.log(typeof(null));
 
 // datatype conversation
 
@@ -35,3 +38,28 @@ console.log(typeof(null));
 // 0  convert in bool  output = false
 // ""  convert in string  output = false
 
+// oppreter
+// a = 1  number
+// b = "B" string
+// c = a+b
+
+// a = 1 + 1 + "B"
+// b = "B" + 1 + 1
+
+// a = 1
+// b = a++  //postfix
+// a = 1
+// b = ++a //prefix
+
+// console.log("a =",a,"b =",b)
+
+// console.log(null>0); false
+// console.log(null>=0); true
+// console.log(null<=0); true
+// console.log(undefined>0); false
+// console.log(true>0); true
+// console.log(true===1); false
+// console.log(false===0); false
+
+// console.log("2"===2); false "strict cheak"
+// console.log("2"==2); true
